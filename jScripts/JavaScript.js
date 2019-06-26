b7bc6f41e2db9caea28d128db5168f55948cd6e4
andromaida <37439077+andromaida@users.noreply.github.com>
2019-06-26 21:05:58 +0300

    Add files via upload

---
+++ jScripts/JavaScript.js
@@ -0,0 +1,22 @@
+﻿//$(document).ready(function () {
+//    $(".spices_fade").mouseenter(function () {
+//        $(".spices_fade").css("opacity", "0.2");
+//    });
+
+//    $(".spices_fade").mouseleave(function () {
+//        $(".spices_fade").css("opacity", "1");
+//    });
+//});
+
+function fade_in(event) {
+    console.log("מה לעזאזאל");
+    document.querySelector("#spices_fade").style.visibility = "visible";
+    event.target.style.zIndex = 2;
+}
+
+function fade_out(event) {
+    console.log("מה cfbgdfghdgfhdgfh");
+    document.querySelector("#spices_fade").style.visibility = "hidden";
+    event.target.style.zIndex = 0;
+
+}
